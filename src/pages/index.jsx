bfac4e130c576fb98c06d3bb5069f558f680754f
ritik jain <ritikraw@gmail.com>
2023-04-23 10:51:31 +0530

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Celebrating Tomato's Birthday</title>
+        <title>Celebrating Tomato's Birthday!</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
